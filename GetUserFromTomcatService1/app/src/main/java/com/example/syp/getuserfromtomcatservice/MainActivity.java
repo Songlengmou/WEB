9a96b47f8d3bfcb264cc9fdd79a8b986cc806004
lengmou <1020492220@qq.com>
2017-06-08 21:13:23 +0800

    syp

---
+++ GetUserFromTomcatService1/app/src/main/java/com/example/syp/getuserfromtomcatservice/MainActivity.java
@@ -0,0 +1,119 @@
+package com.example.syp.getuserfromtomcatservice;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.syp.getuserfromtomcatservice.models.User;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RequestQueue requestQueue;
+    private FloatingActionButton fab;
+    private TextView outPutText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        outPutText = (TextView) findViewById(R.id.outputText);
+
+        requestQueue = Volley.newRequestQueue(this);
+
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        requestQueue.add(new JsonObjectRequest(
+                "http://10.0.2.2:8080/getuser"
+                , null,
+                new Response.Listener<JSONObject>() {
+
+                    private String gender;
+                    private String age;
+                    private String name;
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        User u = new User(response);
+                        JSONArray arr = null;
+                        try {
+                            arr = new JSONArray(u.toString());
+                            System.out.println(u.toString());
+                            for (int i = 0; i < arr.length(); i++) {
+                                JSONObject temp = (JSONObject) arr.get(i);
+                                name = temp.getString("name");
+                                age = temp.getString("age");
+                                gender = temp.getString("gender");
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+//                        outPutText.setText(String.format("User[mame=%s,age=%d,gender=%s]", u.getName(), u.getAge(), u.getGender()));
+                        outPutText.setText(String.format("User[mame=%s,age=%d,gender=%s]", name,age,gender));
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Snackbar.make(fab, "连接网络失败", Snackbar.LENGTH_SHORT).show();
+                    }
+                }
+        ));
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ GetUserFromTomcatService1/app/src/main/java/com/example/syp/getuserfromtomcatservice/models/User.java
@@ -0,0 +1,85 @@
+package com.example.syp.getuserfromtomcatservice.models;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by syp on 17-5-22.
+ */
+public class User extends AbstractModel {
+
+    private String name;
+    private int age;
+    private String gender;
+
+    public User() {
+    }
+
+    public User(JSONObject jsonObject) {
+        super(jsonObject);
+    }
+
+    public User(String name, int age, String gender) {
+        setName(name);
+        setAge(age);
+        setGender(gender);
+    }
+
+    public User(String json) throws JSONException {
+        super(json);
+    }
+
+    public String getName() {
+        try {
+            return gettoJsonObject().getString("name");
+        } catch (JSONException e) {
+            return null;
+        }
+    }
+
+    public void setName(String name) {
+        try {
+            gettoJsonObject().put("name", name);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 获取用户名字，如果没有该字段，则返回-1
+     *
+     * @return
+     */
+    public int getAge() {
+        try {
+            return gettoJsonObject().getInt("age");
+        } catch (JSONException e) {
+            return -1;
+        }
+    }
+
+    public void setAge(int age) {
+        try {
+            gettoJsonObject().put("age", age);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getGender() {
+        try {
+            return gettoJsonObject().getString("gender");
+        } catch (JSONException e) {
+            return null;
+        }
+    }
+
+    public void setGender(String gender) {
+        try {
+            gettoJsonObject().put("gender", gender);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
